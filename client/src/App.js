84514104aeab6ff46f8b6fb4c31da70846aea54c
DingoDang1113 <Shalpeng109@gmail.com>
2023-11-20 19:37:21 -0800

    fix Dashboard setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
-import { Dashboard } from "@mui/icons-material";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
-import { UseSelector, useSelector } from "react-redux/es/hooks/useSelector";
+import { useSelector } from "react-redux/es/hooks/useSelector";
 import { BrowserRouter, Navigate } from "react-router-dom";
 import { themeSettings } from "theme";
+import { Route, Routes } from "react-router-dom";
 
 import Layout from "scenes/layout";
-import Dashboard from "scenes/dashboard";
+import Dashboard  from "scenes/dashboard";
+
 
 
 
