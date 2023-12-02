053c012944da70146ce5e8a6ea4fe3035db96a00
DingoDang1113 <Shalpeng109@gmail.com>
2023-12-01 19:32:33 -0800

    building side bar

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -7,7 +7,17 @@ import NavBar from "components/Navbar";
 import SideBar from "components/Sidebar";
 
 const Layout = () => {
-  return <Box width="100%" height="100%">
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+
+  return <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+    <SideBar 
+      isNonMobile = {isNonMobile}
+      drawerWidth = "250px"
+      isSidebarOpen = {isSidebarOpen}
+      setIsSidebarOpen = {setIsSidebarOpen}
+    />
     <Box>
       <NavBar />
       <Outlet />  //where dashboard will be located
