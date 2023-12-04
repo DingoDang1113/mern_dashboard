62efebd7b613a362e1d87343b9a6f119d7accff2
DingoDang1113 <Shalpeng109@gmail.com>
2023-12-03 17:22:52 -0800

    update index file to include User

--- server/index.js
+++ server/index.js
@@ -11,6 +11,11 @@ import generalRoutes from "./routes/general.js";
 import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
 
+// data imports 
+import User from "./models/User.js";
+import { dataUser } from "./data/index.js"; 
+
+
 
 /* CONFIGURATION */
 dotenv.config();
@@ -38,7 +43,8 @@ mongoose
     useUnifiedTopology: true,
 })
 .then(() => {
-    app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    
 })
 .catch((error ) => {
     console.log(`${error} did not connect`)
