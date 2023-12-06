172a8ba2c27e85c6eb0b081f957a59ba2fac8ff5
DingoDang1113 <Shalpeng109@gmail.com>
2023-12-05 19:35:27 -0800

    update index file to import Product & ProductStat

--- server/index.js
+++ server/index.js
@@ -13,7 +13,9 @@ import salesRoutes from "./routes/sales.js";
 
 // data imports 
 import User from "./models/User.js";
-import { dataUser } from "./data/index.js"; 
+import Product from './models/product.js';
+import ProductStat from './models/ProductStat.js';
+import { dataUser, dataProduct, dataProductStat } from "./data/index.js"; 
 
 
 
@@ -46,6 +48,8 @@ mongoose
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
     
     /* Only Add Data One Time */
+    Product.insertMany(dataProduct);
+    ProductStat.insertMany(dataProductStat);
 
     // User.insertMany(dataUser);
 })
