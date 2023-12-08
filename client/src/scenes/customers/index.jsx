bdeea0adfaa3216a877cc13b30869ab594b8cced
DingoDang1113 <Shalpeng109@gmail.com>
2023-12-07 17:35:26 -0800

    add customer table

--- client/src/scenes/customers/index.jsx
+++ client/src/scenes/customers/index.jsx
@@ -10,11 +10,62 @@ const Customers = () => {
     const { data, isLoading} = useGetCustomersQuery();
     // console.log("customers", data)
 
+    const columns = [
+        {
+            field: "_id",
+            headerName: "ID",
+            flex: 1, 
+        }, 
+        {
+            field: "name",
+            headerName: "Name",
+            flex: 0.5,
+        }, 
+        {
+            field: "email",
+            headerName: "Email",
+            flex: 1,
+        }, 
+        {
+            field: "phoneNumber",
+            headerName: "Phone",
+            flex: 0.5,
+            renderCell: (params) => {
+                return params.value.replace(/^(\d{3})(\d{3})(\d{4})/, "($1)$2-$3")
+            }
+        }, 
+        {
+            field: "country",
+            headerName: "Country",
+            flex: 0.4,
+        }, 
+        {
+            field: "occupation",
+            headerName: "Occupation",
+            flex: 1,
+        }, 
+        {
+            field: "role",
+            headerName: "Role",
+            flex: 0.5,
+        }, 
+ 
+    ];
+
   return <Box m="1.5rem 2.rem">
     <Header title="CUSTOMERS" subtitle="A list of Customers" />
+    <Box>
+        <DataGrid
+            loading = {isLoading || !data}
+            getRowId={(row) => row._id}
+            rows={data || []}
+            columns = {columns}
+            
+        />
+    </Box>
 
 
   </Box>
 }
 
-export default Customers
+export default Customers;
