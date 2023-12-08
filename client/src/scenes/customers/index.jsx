6fed4df28626d0a52ea5e01a4beb204724c06c4a
DingoDang1113 <Shalpeng109@gmail.com>
2023-12-07 17:41:05 -0800

    formatting customer table

--- client/src/scenes/customers/index.jsx
+++ client/src/scenes/customers/index.jsx
@@ -14,7 +14,7 @@ const Customers = () => {
         {
             field: "_id",
             headerName: "ID",
-            flex: 1, 
+            flex: 0.8, 
         }, 
         {
             field: "name",
@@ -54,7 +54,10 @@ const Customers = () => {
 
   return <Box m="1.5rem 2.rem">
     <Header title="CUSTOMERS" subtitle="A list of Customers" />
-    <Box>
+    <Box mt="40px" height="75vh"
+    
+    
+    >
         <DataGrid
             loading = {isLoading || !data}
             getRowId={(row) => row._id}
