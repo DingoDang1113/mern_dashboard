3bd23e8efad8abbd191449f638a4281b224dfecb
DingoDang1113 <Shalpeng109@gmail.com>
2023-12-11 09:10:40 -0800

    build getTransactions

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -44,9 +44,30 @@ export const getCustomers = async(req, res) => {
 export const getTransactions = async(req, res) => {
 
     try {
-        const transactions = await Transaction.find();
+        // Server side pagination
+        // sort should look like this {"field":"userId", "sort":"desc"} from frontend 
+        const { page = 1, pageSize = 20, sort = null, search = ""} = req.query;
 
-        res.status(200).json(transactions)
+        // formatted sort should look like {userId: -1} -> MongoDB should read
+        const generateSort = () => {
+            const sortParsed = JSON.parse(sort);
+            const sortFormatted = {
+                [sortParsed.field]: (sortParsed.sort = "asc" ? 1 : -1),
+            }; 
+
+            return sortFormatted;
+        }
+
+        const sortFormatted = Boolean(sort) ? generateSort() : {};
+
+        const transactions = await Transaction.find({
+            $or: [
+                { cost: { $regex: new RegExp(search, "i")}}, 
+                { userId: { $regex: new RegExp(search, "i") }}
+            ],
+        });
+
+        res.status(200).json()
 
     } catch(error) {
         res.status(404).json({ message:error.message });
--- server/models/Transaction.js
+++ server/models/Transaction.js
@@ -1,14 +1,17 @@
-import React from 'react';
 import mongoose from 'mongoose';
 
 
 const TransactionSchema = new mongoose.Schema (
     {
-        transactionId: String, 
-            
+        userId: String, 
+        cost: String,
+        products: {
+            type: [mongoose.Types.ObjectId],
+            of: Number,
+        },
     }, 
     {timestamps: true}
 );
 
-const Transactions = mongoose.model("Transactions", TransactionSchema);
-export default Transactions;
+const Transaction = mongoose.model("Transaction", TransactionSchema);
+export default Transaction;
