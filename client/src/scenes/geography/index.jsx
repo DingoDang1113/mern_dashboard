640af420e2189598966b9f7ca12c32f49fa6e5c2
DingoDang1113 <Shalpeng109@gmail.com>
2023-12-20 11:44:55 -0800

    set up map

--- client/src/scenes/geography/index.jsx
+++ client/src/scenes/geography/index.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import {Box, useTheme } from "@mui/material";
 import { useGetGeographyQuery } from 'state/api';
 import Header from 'components/Header';
+import { ResponsiveChoropleth } from '@nivo/geo';
+import { geoData } from 'state/geoData'; 
+
 
 const Geography  = () => {
   return (
