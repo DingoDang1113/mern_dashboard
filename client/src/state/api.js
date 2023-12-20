5ef75139c29a0aee47527f558a6298ba766c9700
DingoDang1113 <Shalpeng109@gmail.com>
2023-12-20 11:29:34 -0800

    set up backend for geography

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -29,9 +29,14 @@ export const api = createApi( {
             providesTags: ["Transactions"],
 
         }), 
+        getGeography: build.query({
+            query: () => "client/geography",
+            providesTags: ["Geography"]
+
+        }),
 
 
     })
 })
 
-export const { useGetUserQuery, useGetProductsQuery, useGetCustomersQuery, useGetTransactionsQuery } = api;
+export const { useGetUserQuery, useGetProductsQuery, useGetCustomersQuery, useGetTransactionsQuery, useGetGeographyQuery} = api;
