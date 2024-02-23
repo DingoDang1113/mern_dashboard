60b178729945c0282d074a7b680f9fefe5058d39
DingoDang1113 <Shalpeng109@gmail.com>
2024-02-23 09:01:47 -0800

    setup admin route

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ import Overview from "scenes/overview";
 import Daily from "scenes/daily";
 import Monthly from "scenes/monthly";
 import Breakdown from "scenes/breakdown";
+import Admin from "scenes/admin";
 
 
 
@@ -42,6 +43,7 @@ function App() {
                 <Route path="/daily" element={<Daily />}/>
                 <Route path="/monthly" element={<Monthly />}/>
                 <Route path="/breakdown" element={<Breakdown />}/>
+                <Route path="/admin" element={<Admin />}/>
 
 
               </Route>
