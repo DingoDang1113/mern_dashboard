c4eae44e30e35cb04e39a62adeda22662ef8b54f
DingoDang1113 <Shalpeng109@gmail.com>
2024-03-15 13:41:21 -0700

    update routes

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
-const express = require('express');
+// const express = require('express');
 
-const app = express();
+// const app = express();
 
-// Middleware function
-app.use((req, res, next) => {
-    next();
-  });
+// // Middleware function
+// app.use((req, res, next) => {
+//     next();
+//   });
 
-// Assign routes
-app.get('/', (req, res) => {
-    if (req.params.message) {
-        res.send(`Echo: ${req.params.message}`);
-    } else {
-        next();
-    }
-});
+// // Assign routes
+// app.get('/', (req, res) => {
+//     if (req.params.message) {
+//         res.send(`Echo: ${req.params.message}`);
+//     } else {
+//         next();
+//     }
+// });
 
-app.get('/', (req, res) => {
-    res.send('Hello from a route handler function!');
-});
+// app.get('/', (req, res) => {
+//     res.send('Hello from a route handler function!');
+// });
 
-app.get('/throw-error', (req, res) => {
-    throw new Error('An error occurred!');
-  });
+// app.get('/throw-error', (req, res) => {
+//     throw new Error('An error occurred!');
+//   });
   
-  // Custom error handler
-  app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(err.status || 500);
-    res.send('An error occurred! Please check the url, or wait a few minutes and try again.');
-});
+//   // Custom error handler
+//   app.use((err, req, res, next) => {
+//     console.error(err);
+//     res.status(err.status || 500);
+//     res.send('An error occurred! Please check the url, or wait a few minutes and try again.');
+// });
 
-// Tell the server to listen for incoming traffic on a specific port
-const port = 3000;
+// // Tell the server to listen for incoming traffic on a specific port
+// const port = 3000;
